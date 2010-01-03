2a7713d6c277bc61f15a84b150130e22d0e14a2f
Jaime Bellmyer <bellmyer@happy-laptop.local>
2010-01-03 11:08:14 -0600

    added shoulda macros for acts_as_indexed and extending should_have_many

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -8,6 +8,7 @@ class ArticleTest < ActiveSupport::TestCase
   should_validate_presence_of :title
   
   should_act_as_list
+  should_act_as_indexed :fields => [:title, :content]
   
   should "be sortable" do
     article = Article.new
@@ -67,6 +68,38 @@ class ArticleTest < ActiveSupport::TestCase
     should "display the first 100 characters of content if type is not documents" do
       article = Article.new(:article_type => 'article', :content => ("abcde" * 100))
       assert_equal ('abcde' * 20) + '...', article.component_preview
+    end
+  end
+  
+  context "finding articles" do
+    setup do
+      @page = Factory.build(:page)
+      @page.id = 1001
+      @page.stubs(:paginate).returns(10)
+    end
+
+    should "show all the articles for this page in a given month, with pagination" do
+      Article.expects(:paginate).with(
+        :page => 1, :per_page => 10, :order => 'created_at DESC',
+        :conditions => ["page_id = ? AND created_at BETWEEN ? AND ?", 1001, DateTime.new(2009, 1, 1), DateTime.new(2009, 1, 31, 11, 59, 59)]
+      ).once
+      Article.find_all_in_month(2009, 1, 1, @page)
+    end
+    
+    should "show all the articles for this page in a given year, with pagination" do
+      Article.expects(:paginate).with(
+        :page => 1, :per_page => 10, :order => 'created_at DESC',
+        :conditions => ["page_id = ? AND created_at BETWEEN ? AND ?", 1001, DateTime.new(2009, 1, 1), DateTime.new(2009, 12, 31, 11, 59, 59)]
+      ).once
+      Article.find_all_in_year(2009, 1, @page)
+    end
+    
+    should "find archive links" do
+      articles = [Factory.build(:article, :created_at => '2008-08-08'), Factory.build(:article, :created_at => '2009-09-09')]
+      Article.expects(:find).with(:all, :select => "created_at", :conditions => ["page_id = ?", 1001]).returns(articles).once
+      
+      results = Article.archive_links(@page)
+      assert_equal [['2009', [articles[1]]], ['2008', [articles[0]]]], results
     end
   end
 end
