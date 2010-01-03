574ed2bdc799b77d3cb17873dd9efcf4a70d1339
Jaime Bellmyer <bellmyer@happy-laptop.local>
2010-01-03 11:06:17 -0600

    expanded should_act_as_list
    it now accepts the same parameters as acts_as_list

--- vendor/gems/seanhussey-woulda-0.2.2/lib/woulda/acts_as_list/macros.rb
+++ vendor/gems/seanhussey-woulda-0.2.2/lib/woulda/acts_as_list/macros.rb
@@ -2,11 +2,38 @@ module Woulda
   module ActsAsList
     module Macros
       # Original source: http://www.soyunperdedor.com/node/34
-      def should_act_as_list
+      def should_act_as_list(options = {})
+        configuration = {:column => :position}
+        configuration.update(options) if options.is_a?(Hash)
+        
         klass = self.name.gsub(/Test$/, '').constantize
 
         context "To support acts_as_list" do
-          should_have_db_column('position', :type => :integer)
+          should_have_db_column(configuration[:column].to_s, :type => :integer)
+
+          if configuration[:scope]
+            if configuration[:scope].is_a?(Symbol)
+              configuration[:scope] = configuration[:scope].to_s
+              configuration[:scope] << '_id' unless configuration[:scope] =~ /_id$/
+
+              should_have_db_column(configuration[:scope], :type => :integer)
+              
+              should "have the correct scope condition when key is nil" do
+                obj = klass.new(configuration[:scope] => nil)
+                assert_equal "#{configuration[:scope]} IS NULL", obj.scope_condition
+              end
+              
+              should "have the correct scope condition when key has a value" do
+                obj = klass.new(configuration[:scope] => 5)
+                assert_equal "#{configuration[:scope]} = 5", obj.scope_condition
+              end
+            else
+              should "have the correct scope condition" do
+                obj = klass.new
+                assert_equal configuration[:scope], obj.scope_condition
+              end
+            end
+          end
         end
 
         should "include ActsAsList methods" do
