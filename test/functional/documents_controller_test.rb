8db751d1d5e38ba239910cfe7d1266c630fc83d3
Jaime Bellmyer <bellmyer@happy-laptop.local>
2010-01-06 00:22:23 -0600

    added functional tests to documents

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -1,8 +1,70 @@
 require 'test_helper'
 
+def should_allow_everything_for(type, user)
+  context "as #{type}" do
+    setup do
+      login_as user if user
+    end
+    
+    context "getting new" do
+      setup do
+        get :new
+      end
+      
+      should_assign_to :document, :class => Document
+      should_respond_with :success
+      should_render_without_layout
+      should_not_set_the_flash
+    end
+    
+    context "creating" do
+      context "with valid data" do
+        setup do
+          Document.any_instance.stubs(:save).returns(true)
+          post :create, :document => {}
+        end
+        
+        should_assign_to :document, :class => Document
+        should_respond_with :success
+        should_render_without_layout
+        should_not_set_the_flash
+      end
+      
+      context "with invalid data" do
+        setup do
+          Document.any_instance.stubs(:save).returns(false)
+          post :create, :document => {}
+        end
+        
+        should_assign_to :document, :class => Document
+        should_respond_with :success
+        should_render_without_layout
+        should_not_set_the_flash
+      end
+    end
+    
+    context "destroying" do
+      setup do
+        @document = Document.new
+        Document.stubs(:find).returns(@document)
+        
+        @document.expects(:destroy).once
+        delete :destroy, :id => 1
+      end
+      
+      should_assign_to(:document){@document}
+      should_respond_with :success
+      should_render_without_layout
+      should_not_set_the_flash
+    end
+  end
+end
+
 class DocumentsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  should_allow_everything_for('admin', :admin)
+  should_allow_everything_for('a member', :quentin)
+  
+  context "as a visitor" do
+    should_require_login_for :new, :create, :destroy
   end
 end
--- test/shoulda_macros/bounce_macros.rb
+++ test/shoulda_macros/bounce_macros.rb
@@ -31,15 +31,36 @@ def should_require_login_for(*tests)
   end
 end
 
-def should_not_authorize(test)
-  model = test_unit_class.class.name.gsub(/ControllerTest$/, '').singularize.constantize
+def should_not_authorize
+  should_not_authorize_for :all
+end
 
-  setup do
-    eval test
-  end
+def should_not_authorize_for(*tests)
+  model = test_unit_class.name.gsub(/ControllerTest$/, '').singularize.constantize
+
+  tests = [:index, :show, :new, :create, :edit, :update, :destroy] if tests.include?(:all)
   
-  should_not_assign_to model.name.to_sym
-  should_respond_with 401
-  should_render_without_layout
-  should_not_set_the_flash
+  tests.each do |test|
+    action = case test
+      when :index   then "get :index"
+      when :show    then "get :show"
+      when :new     then "get :new"
+      when :create  then "post :create"
+      when :edit    then "get :edit"
+      when :update  then "put :update"
+      when :destroy then "delete :destroy"
+      else test
+    end
+  
+    context "attempting to #{action}" do
+      setup do
+        eval action
+      end
+      
+      should_not_assign_to model.name.to_sym
+      should_respond_with 401
+      should_render_without_layout
+      should_not_set_the_flash
+    end
+  end
 end
