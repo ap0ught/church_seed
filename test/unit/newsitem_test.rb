f964ca5e3a9d97c4e32e9f1e05ca91c883d0d875
Jaime Bellmyer <bellmyer@happy-laptop.local>
2010-01-13 20:37:08 -0600

    added unit tests for newsitems

--- test/unit/newsitem_test.rb
+++ test/unit/newsitem_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class NewsitemTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  should "only find newsitem articles" do
+    included = Article.create(:title => 'included', :article_type => 'news', :type => 'Newsitem')
+    excluded = Article.create(:title => 'excluded', :article_type => 'article')
+    assert_equal 2, Article.count
+    
+    assert_equal [included.id], Newsitem.find(:all).map(&:id)
+  end
+
+  should "not be sortable" do
+    assert !Newsitem.new.sortable?
   end
 end
